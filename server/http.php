c80592a2fd5195a2ff8cd74e2e62b164e6a20bfb
tongyu110 <pengchengxue110@163.com>
2018-05-17 05:30:51 +0800

    add http server

---
+++ server/http.php
@@ -0,0 +1,25 @@
+<?php
+
+echo 'shell start';
+
+define('__ROOT__', realpath('.'));
+
+$http = new swoole_http_server("127.0.0.1", 9501);
+$http->set([
+    'worker_num'=>4,
+    'max_request'=>50
+]);
+
+$http->on('request', function ($request, $response) {
+    //var_dump($request->get, $request->post);
+    echo 'echo request';
+    $response->header("Content-Type", "text/html; charset=utf-8");
+    $response->end(getTplContent());
+});
+$http->start();
+
+function getTplContent() {
+    $file = __ROOT__ . '/Template/index.html';
+    $content = file_get_contents($file);
+    return $content;
+}
