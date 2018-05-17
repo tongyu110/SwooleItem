3076f30d26bcd156c91d7942abe4397e2a24956b
tongyu110 <pengchengxue110@163.com>
2018-05-17 17:03:44 +0800

    add http server

--- server/http.php
+++ server/http.php
@@ -22,9 +22,11 @@ $http->on('request', function ($request, $response) {
 });
 
 $http->on('Receive',function($server, $fd, $from_id, $data) {
+   echo "tick start \n";
    $server->tick(1000, function() use ($server, $fd) {
         echo $fd . "\n";
     });
+   echo "tick end \n";
 });
 
 $http->start();
