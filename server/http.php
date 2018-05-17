8d3e56d2d7aacb6786bee48732ce4a3b2d94ede1
tongyu110 <pengchengxue110@163.com>
2018-05-17 17:12:45 +0800

    add http server

--- server/http.php
+++ server/http.php
@@ -21,7 +21,7 @@ $http->on('request', function ($request, $response) {
     $response->end(getTplContent());
 });
 
-$http->on('Receive',function($server, $fd, $from_id, $data) {
+$http->on('receive',function($server, $fd, $from_id, $data) {
    echo "tick start \n";
    $server->tick(1000, function() use ($server, $fd) {
         echo $fd . "\n";
