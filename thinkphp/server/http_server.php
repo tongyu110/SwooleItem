85232bea366dd99fa574059dc3068c2a28d30f09
tongyu110 <pengchengxue110@163.com>
2018-05-23 01:04:52 +0800

    up http server

--- thinkphp/server/http_server.php
+++ thinkphp/server/http_server.php
@@ -9,10 +9,19 @@ $http = new swoole_http_server("0.0.0.0", 8811);
 
 $http->set(
     [
+        'worker_num'=>4,
+        'max_request'=>50,
         'enable_static_handler' => true,
         'document_root' => "/home/Dev/SwooleItem/thinkphp/public/static",
     ]
 );
+
+$thtp->on('WorkerStart',function($serv, $worker_id){
+    
+    define('APP_PATH', __DIR__ . '/../application/');
+    require __DIR__ . '/../thinkphp/base.php';
+});
+
 $http->on('request', function($request, $response) {
     $response->cookie("singwa", "xsssss", time() + 1800);
     $response->end("sss". json_encode($request->get));
