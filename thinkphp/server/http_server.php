3fc788662992882fe002caabf378ac23be351527
tongyu110 <pengchengxue110@163.com>
2018-05-23 01:33:30 +0800

    up http server

--- thinkphp/server/http_server.php
+++ thinkphp/server/http_server.php
@@ -17,14 +17,47 @@ $http->set(
 );
 
 $http->on('WorkerStart',function($serv, $worker_id){
-    
     define('APP_PATH', __DIR__ . '/../application/');
     require __DIR__ . '/../thinkphp/base.php';
 });
 
 $http->on('request', function($request, $response) {
-    $response->cookie("singwa", "xsssss", time() + 1800);
-    $response->end("sss". json_encode($request->get));
+    
+    
+    if(isset($request->server)) {
+        foreach(($request->server as $k=>$v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    
+    if(isset($request->header)) {
+        foreach(($request->header as $k=>$v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    
+    if(isset($request->get)) {
+        foreach(($request->get as $k=>$v) {
+            $_GET[$k] = $v;
+        }
+    }
+    
+    if(isset($request->post)) {
+        foreach(($request->post as $k=>$v) {
+            $_POST[$k] = $v;
+        }
+    }
+    
+    ob_start();
+    // 执行应用并响应
+    think\Container::get('app', [APP_PATH])
+    ->run()
+    ->send();
+    
+    $re = ob_get_contents();
+    ob_end_clean();
+    
+    $response->end($re);
 });
 
 $http->start();
